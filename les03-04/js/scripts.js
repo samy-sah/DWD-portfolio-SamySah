81e69c0ae3789673774105597b537496347a4bdd
samy-sah <55875247+samy-sah@users.noreply.github.com>
2020-03-11 17:56:25 +0100

    van

--- les03-04/js/scripts.js
+++ les03-04/js/scripts.js
@@ -8,6 +8,9 @@
 let menu = document.querySelectorAll('.menu a');
 let rngSize = document.querySelector('#rngSize');
 let imgBig = document.querySelector('#imgBig');
-menu.addEventListener('change', function() {
-	imgBig.style.fontSize = this.value;
+menu.addEventListener('click', function() {
+	imgBig.style.filter = grayscale(100%);
+});
+rngSize.addEventListener('input', function() {
+	imgBig.style.filter = opacity(this.value);
 });
