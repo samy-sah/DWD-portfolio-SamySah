38505f158e7aadc973dd4bca1bcb46a5b6197583
samy-sah <55875247+samy-sah@users.noreply.github.com>
2020-04-19 20:32:03 +0200

    Project

--- Project Game/js/scripts.js
+++ Project Game/js/scripts.js
@@ -4,12 +4,20 @@
 *@author Samy Sah samy.sah@student.odisee.be>
 *
 */
-(function() {
-  'use strict';
-  window.addEventListener('load', function(){
-    const square = document.querySelectorAll('.square')
-    const molS = document.querySelectorAll('.molS')
-    const tijd = document.querySelector('#tijd')
-    let score = document.querySelector('#score')
-  });
-});
+;(function(){
+	'use strict';
+
+  let btnStart = document.querySelector('#btnStart');
+  let btnPause = document.querySelector('#btnPause');
+  let btnReset = document.querySelector('#btnReset');
+
+  const grid = document.querySelectorAll('.grid-item')
+  const molS = document.querySelectorAll('.molS')
+  const mod = document.querySelectorAll('.mod');
+  let score = document.querySelector('#score')
+  const tijd = document.querySelector('#tijd')
+
+  let result = 0
+  let currentTime = tijd.textContent
+  
+})();
