7ec85b1f5f2fab4a18bec695f63d08d3dc1e65be
samy-sah <55875247+samy-sah@users.noreply.github.com>
2020-05-17 17:38:05 +0200

    project

--- Project Edit/js/scripts.js
+++ Project Edit/js/scripts.js
@@ -5,50 +5,42 @@
 *
 */
 (function(){
-	'use strict';
+    'use strict';
+    // =====================================
+	// Filters en sliders
+    // =====================================
 
-    // =====================================
-	// Titel effect
-    // =====================================
-    const title = document.querySelector('.effect');
-    const text = title.textContent;//Om de titel van de pagina te kunnen opvragen
-    const divideText = text.split("");//Begin van onze effect
-    title.textContent = "";
-
-    for(let i=0; i< divideText.length; i++){ // om een loop te kunnen creeren
-        title.innerHTML += "<span>" + divideText[i] + "</span>";
-    }
-    
-    let letter = 0; // Om te starten vanaf de eerste letter namelijk "E"
-    let onDo = function(){
-        const span = title.querySelectorAll('span')[letter];
-        span.classList.add('vervang');
-        letter++
-        if(letter === divideText.length){
-            end();
-        }
-    }
-    let timer = setInterval(onDo, 65);
-    let end = function(){
-        clearInterval(timer);
-        timer = null;
-    }
-
-    // =====================================
-	// Filters
-    // =====================================
-    
+    let slider = document.querySelectorAll('.slider');
     let save = document.querySelector('#save');
     let reset = document.querySelector('#reset');
-	let filter1 = document.querySelector('#normal');
-	let filter2 = document.querySelector('#grayscale');
+    let filter1 = document.querySelector('#normal');
+    let filter2 = document.querySelector('#grayscale');
 	let filter3 = document.querySelector('#brightness');
 	let filter4 = document.querySelector('#blur');
     let filter5 = document.querySelector('#hue');
     let filter6 = document.querySelector('#sepia');
 	let rngSize = document.querySelector('#rngSize');
-    let percent = document.querySelector('#percent');
+    let sliderVal = document.querySelector('#slider1-val');
+    let color = document.querySelector('#inpColor');
+    
+    let applyAll = function() {
+        inFoto.style.color = color.value;
+    }
+    // attach events
+    color.addEventListener('change', applyAll);
 
+    rngSize.addEventListener('input', function(){
+		sliderVal.innerHTML = parseInt(this.value * 100) + '%';
+		inFoto.style.opacity = this.value;
+    });
+
+    reset.addEventListener('click', function(e) {
+        e.preventDefault();
+        slider.innerHTML = rngSize.value = '0';
+        sliderVal.innerHTML = '';
+        inFoto.style = '';
+    });
+    
     // =====================================
 	// Images loader  
     // =====================================
@@ -56,12 +48,11 @@
     window.addEventListener('load', function(){
         let btn = document.querySelector('#btn');
         let inFoto = document.getElementById('inFoto');
-        let slider = document.querySelectorAll('.slider');
         btn.addEventListener('click', function(e){
             e.preventDefault();
-            let url = document.getElementById('url');    
+            let url = document.getElementById('url');
             let urliVal = url.value;
-            inFoto.setAttribute('src',urliVal);                
+            inFoto.setAttribute('src',urliVal);
         });
     });
     let myfile = document.querySelector('#myfile');
