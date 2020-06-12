202befac24e224970b0381bcf9b9d8623a9edc92
samy-sah <55875247+samy-sah@users.noreply.github.com>
2020-06-12 20:09:53 +0200

    les 01-02 en project

--- Project Edit/js/scripts.js
+++ Project Edit/js/scripts.js
@@ -36,20 +36,27 @@
     // =====================================
 	// Filters en sliders
     // =====================================
-
     let slider = document.querySelectorAll('.slider');
     let save = document.querySelector('#save');
     let reset = document.querySelector('#reset');
-    let filter1 = document.querySelector('#normal');
-    let filter2 = document.querySelector('#grayscale');
-	let filter3 = document.querySelector('#brightness');
-	let filter4 = document.querySelector('#blur');
-    let filter5 = document.querySelector('#hue');
-    let filter6 = document.querySelector('#sepia');
+    let grayscale = document.getElementById('grayscale');
+	let brightness = document.getElementById('brightness');
+	let blur = document.getElementById('blur');
+    let hue = document.getElementById('hue');
+    let sepia = document.getElementById('sepia');
 	let rngSize = document.querySelector('#rngSize');
     let sliderVal = document.querySelector('#slider1-val');
     let color = document.querySelector('#inpColor');
-    
+
+    let effect = function(){  	
+        let gray = grayscale.value;    
+        let bri = brightness.value;    
+        inFoto.style.filter = 'grayscale('+gray+'%) brightness('+bri+'%)';        	
+    }
+    document.getElementById('grayscale').addEventListener('input', effect);
+    document.getElementById('brightness').addEventListener('input', effect);
+
+
     rngSize.addEventListener('input', function(){
 		sliderVal.innerHTML = parseInt(this.value * 100) + '%'; // change the content
 		inFoto.style.opacity = this.value; // change css properties
@@ -60,6 +67,8 @@
         slider.innerHTML = rngSize.value = '0';
         sliderVal.innerHTML = '';
         inFoto.style = '';
+        grayscale.value = '0';
+        brightness.value = '0';
     });
     
     // =====================================
