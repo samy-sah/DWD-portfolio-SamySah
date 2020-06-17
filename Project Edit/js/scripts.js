ef687bf2db4b077d198b672573c1207acbc5b001
samy-sah <55875247+samy-sah@users.noreply.github.com>
2020-06-17 21:31:30 +0200

    project

--- Project Edit/js/scripts.js
+++ Project Edit/js/scripts.js
@@ -6,7 +6,6 @@
 */
 (function(){
     'use strict';
-    
     // =====================================
     // Titel effect 
     // =====================================
@@ -28,68 +27,103 @@
             end();
         }
     }
-    let timer = setInterval(onDo, 65);
+    let timer = setInterval(onDo, 65); // timer zodat de het stopt
     let end = function(){
         clearInterval(timer);
         timer = null;
     }
     // =====================================
-	// Filters en sliders
+    // Filters en sliders
+    // https://www.tutorialspoint.com/How-to-set-the-brightness-and-contrast-of-an-image-with-JavaScript
+    // https://stackoverflow.com/questions/36518150/changing-css-filter-values-via-javascript
     // =====================================
-    let slider = document.querySelectorAll('.slider');
-    let save = document.querySelector('#save');
+    let img = document.getElementById('inFoto');  // element with id 'inFoto'
+    let canvases = document.querySelector('canvas');
+    let slider = document.querySelectorAll('.slider'); 
     let reset = document.querySelector('#reset');
     let grayscale = document.getElementById('grayscale');
 	let brightness = document.getElementById('brightness');
 	let blur = document.getElementById('blur');
-    let hue = document.getElementById('hue');
+    let invert = document.getElementById('invert');
     let sepia = document.getElementById('sepia');
-	let rngSize = document.querySelector('#rngSize');
-    let sliderVal = document.querySelector('#slider1-val');
-    let color = document.querySelector('#inpColor');
+    let contrast = document.getElementById('contrast');
+    let opacity = document.getElementById('opacity');
 
-    let effect = function(){  	
-        let gray = grayscale.value;    
-        let bri = brightness.value;    
-        inFoto.style.filter = 'grayscale('+gray+'%) brightness('+bri+'%)';        	
+    // apply filters
+    let effect = function(){
+        let bri = brightness.value;
+        let gray = grayscale.value;
+        let blu = blur.value;
+        let inv = invert.value
+        let sep = sepia.value
+        let con = contrast.value
+        let op = opacity.value
+        
+        img.style.filter = 'brightness('+bri+'%) grayscale('+gray+'%) blur('+blu+'px) invert('+inv+'%) sepia('+sep+'%) contrast('+con+'%) opacity('+op+'%)';        	
     }
     document.getElementById('grayscale').addEventListener('input', effect);
     document.getElementById('brightness').addEventListener('input', effect);
+    document.getElementById('blur').addEventListener('input', effect);
+    document.getElementById('invert').addEventListener('input', effect);
+    document.getElementById('sepia').addEventListener('input', effect);
+    document.getElementById('contrast').addEventListener('input', effect);
+    document.getElementById('opacity').addEventListener('input', effect);
 
-
-    rngSize.addEventListener('input', function(){
-		sliderVal.innerHTML = parseInt(this.value * 100) + '%'; // change the content
-		inFoto.style.opacity = this.value; // change css properties
-    });
-
+    // reset sliders and canvas
     reset.addEventListener('click', function(e) {
         e.preventDefault();
-        slider.innerHTML = rngSize.value = '0';
-        sliderVal.innerHTML = '';
-        inFoto.style = '';
+        img.style = '';
         grayscale.value = '0';
-        brightness.value = '0';
+        brightness.value = '100';
+        blur.value = '0';
+        invert.value = '0';
+        sepia.value ='0';
+        contrast.value ='100';
+        opacity.value ='100';
+        let context = canvases.getContext('2d');
+        context.clearRect(10, 10, canvases.width, canvases.height);
+    });
+
+    // store image in localstorage
+    img.addEventListener('load', function() {
+      localStorage.setItem("todoData", this.src);
+    });   
+    if (localStorage.getItem("todoData")) {
+      img.src = localStorage.getItem("todoData");
+      
+    }
+
+    // remove localstorage
+    remove.addEventListener('click', function(){
+        img.src = localStorage.removeItem("todoData");
     });
     
+    // save filter and put it into the canvas
+    document.getElementById('saveEffect').addEventListener('click', function() {
+        let context = canvases.getContext('2d');
+        context.clearRect(10, 10, canvases.width, canvases.height);
+        context.filter = getComputedStyle(img).getPropertyValue('filter');
+        context.drawImage(img,10, 10, img.width, img.height);
+    });
+
+    // download the image into your folder.
+    let save = document.querySelector('#save');
+    save.addEventListener('click', function(){
+        const a = document.createElement("a");
+        a.href = canvases.toDataURL();
+        a.download = "image.png";
+        document.body.appendChild(a);
+        a.click();
+        document.body.removeChild(a);
+    });
+
+
     // =====================================
 	// Images loader  
-    // =====================================
-    
-    window.addEventListener('load', function(){
-        let btn = document.querySelector('#btn');
-        let inFoto = document.getElementById('inFoto'); // element with id 'inFoto'            
-        let url = document.getElementById('url'); // element with id 'url' 
-        btn.addEventListener('click', function(e){
-            e.preventDefault();
-            let urliVal = url.value; // displays the URL of the URL field.
-            inFoto.src = urliVal;
-        });
-    });
-    // LOCALE STORAGE NOG MAKEN EN OPZOEKEN !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    // =====================================    
     let myfile = document.querySelector('#myfile');
     myfile.addEventListener('change', function() {
-        let url = URL.createObjectURL(myfile.files[0]); // A File, Blob or MediaSource object for which an object URL will be created.
-        // returns a File object at the index 0
-        inFoto.src = url; // displays the value of the src attribute of the image.
+        let url = URL.createObjectURL(myfile.files[0]); // A File, Blob or MediaSource object for which an object URL will be created. // returns a File object at the index 0
+        img.src = url; // displays the value of the src attribute of the image.
     });
 })();
